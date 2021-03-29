9124867c6a661d4c56828023ad77a37ee545a53b
kimgyoungjin98 <gyu250@naver.com>
2021-03-29 16:42:19 +0900

    score

---
+++ Java_009_Application/src/com/callor/apps/Score_01.java
@@ -0,0 +1,17 @@
+package com.callor.apps;
+
+import com.callor.apps.service.ScoreServiceV1;
+
+public class Score_01 {
+
+	public static void main(String[] args) {
+		
+		ScoreServiceV1 ssV1 = new ScoreServiceV1();
+		
+		ssV1.score();
+		ssV1.total();
+		ssV1.printScore();
+		
+	}
+	
+}
---
+++ Java_009_Application/src/com/callor/apps/service/ScoreServiceV1.java
@@ -0,0 +1,84 @@
+package com.callor.apps.service;
+
+import java.util.Scanner;
+
+import com.callor.apps.LinesService;
+
+public class ScoreServiceV1 {
+	
+	Scanner scan = new Scanner(System.in);
+	 
+	int[] intKor;
+	int[] intEng;
+	int[] intMath;
+
+	int[] intSum;
+	float[] floatAvg;
+
+	int totalKor;
+	int totalEng;
+	int totalMath;
+	int total;
+	float avg;
+
+	public ScoreServiceV1() {
+
+		intKor = new int[5];
+		intEng = new int[5];
+		intMath = new int[5];
+		intSum = new int[5];
+		floatAvg = new float[5];
+	}
+
+	public void score() {
+		System.out.println(LinesService.dLines(100));
+		for (int i = 0; i < intKor.length; i++) {
+			System.out.printf("학생 %d 국어성적 : ", i + 1);
+			intKor[i] = scan.nextInt();
+			if(intKor[i] > 100) {
+				System.out.println("100보다 작은 수를 입력해주세요");
+			}else if(intKor[i] < 0) {
+				System.out.println("0보다 큰 수를 입력해주세요");
+			}
+			System.out.printf("학생 %d 영어성적 : ", i + 1);
+			intEng[i] = scan.nextInt();
+			System.out.printf("학생 %d 수학성적 : ", i + 1);
+			intMath[i] = scan.nextInt();
+		}
+		System.out.println(LinesService.dLines(100));
+	}
+
+	public void total() {
+		for (int i = 0; i < intKor.length; i++) {
+			intSum[i] = intKor[i] + intEng[i] + intMath[i];
+			floatAvg[i] = (float) intSum[i] / 3;
+			totalKor += intKor[i];
+			totalEng += intEng[i];
+			totalMath += intMath[i];
+		}
+		total = totalKor + totalEng + totalMath;
+		avg = (float) total / 3;
+
+	}
+
+	public void printScore() {
+
+		System.out.println("학생 성적 리스트");
+		System.out.println(LinesService.dLines(100));
+		for (int i = 0; i < intKor.length; i++) {
+			System.out.printf("학생%d 국어성적 : %d\t| ", (i + 1), intKor[i]);
+			System.out.printf("학생%d 영어성적 : %d\t| ", (i + 1), intEng[i]);
+			System.out.printf("학생%d 수학성적 : %d\t| ", (i + 1), intMath[i]);
+			System.out.printf("학생%d 총점 : %d\t| ", (i + 1), intSum[i]);
+			System.out.printf("학생%d 평균 : %3.2f\n", (i + 1), floatAvg[i]);
+		}
+		System.out.println(LinesService.dLines(100));
+		System.out.printf("국어 총합 : %d\t| ", totalKor);
+		System.out.printf("영어 총합 : %d\t| ", totalEng);
+		System.out.printf("수학 총합 : %d\t| ", totalMath);
+		System.out.printf("과목 총합 : %d\t| ", total);
+		System.out.printf("총합 평균 : %3.2f", avg);
+
+	}
+
+}
